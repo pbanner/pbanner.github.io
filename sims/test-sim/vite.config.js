cde5115338441d70e7a16ed18772839d14e472c9
Patrick Banner <53546824+pbanner@users.noreply.github.com>
2026-06-03 10:43:40 -0400

    Update for Safari compatibility

--- sims/test-sim/vite.config.js
+++ sims/test-sim/vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    legacy({
+      targets: ['defaults', 'safari >= 13', 'ios >= 13'],
+    }),
+  ],
   base: '/sims/test-sim/dist/',
 })
