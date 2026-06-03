f1897422aa6183835a98ff1d33d87a0336eb1d0b
Patrick Banner <53546824+pbanner@users.noreply.github.com>
2026-06-03 11:10:22 -0400

    Revert "Update for Safari compatibility"
    This reverts commit cde5115338441d70e7a16ed18772839d14e472c9.

--- sims/test-sim/vite.config.js
+++ sims/test-sim/vite.config.js
@@ -1,14 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import legacy from '@vitejs/plugin-legacy'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    legacy({
-      targets: ['defaults', 'safari >= 13', 'ios >= 13'],
-    }),
-  ],
+  plugins: [react()],
   base: '/sims/test-sim/dist/',
 })
